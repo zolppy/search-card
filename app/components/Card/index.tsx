04096f920f42c691bd487b2c4c0e5675c8813b8b
Gabriel Cavalcante <gabriel.lcifba@gmail.com>
2024-08-27 01:58:17 -0300

    fix: alterada ordem de card em mobile

--- app/components/Card/index.tsx
+++ app/components/Card/index.tsx
@@ -9,7 +9,7 @@ interface ICard {
 
 export default function Card({ name, number, date, cvv }: ICard) {
   return (
-    <div className="flex flex-col gap-4 min-w-[318px] w-full max-w-[430px]">
+    <div className="flex flex-col gap-4 min-w-[318px] w-full max-w-[430px] order-first md:order-last">
       <H3>Visualização</H3>
       <div className="flex flex-col gap-5 bg-black text-white p-3 rounded-lg min-w-[318px] max-w-[430px] min-h-[208px] justify-center">
         <h1 className="text-3xl">Visa</h1>
